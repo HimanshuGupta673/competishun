8159cd0de383d13cc388ae84ba3d27bcd0586037
HimanshuGupta673 <himanshuu673@gmail.com>
2023-10-09 18:40:31 +0530

    priority color

--- client/src/components/HomePageComp/Todo.js
+++ client/src/components/HomePageComp/Todo.js
@@ -45,10 +45,11 @@ const Todo = ({ todo }) => {
                 style={{
                     textDecoration: todo.done ? 'line-through' : '',
                     color: todo.done && !editing ? '#bdc3c7' :
-                        todo.priority === 'high' ? '#f76880' :
-                            todo.priority === 'medium' ? '#eef78b' :
                                 '#34495e',
-                    backgroundColor: todo.done && !editing ? '#20B2AA' : '#34495e'
+                    backgroundColor: todo.done && !editing ? '#bdc3c7' :
+                    todo.priority === 'high' ? '#f76880' :
+                        todo.priority === 'medium' ? '#eef78b' :
+                            '#34495e',
 
                 }}
                 data-testid="todo-test"
